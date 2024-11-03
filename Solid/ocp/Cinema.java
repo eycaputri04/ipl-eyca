43b66a66a92d394e004b68c9c1144f8ccc6a335a
DESKTOP-RRNUSDH\USER <eycaputri04@gmail.com>
2024-11-03 18:59:49 +0700

    penerapan ocp

--- Solid/ocp/Cinema.java
+++ Solid/ocp/Cinema.java
@@ -1,3 +1,5 @@
 public class Cinema {
     public Double price;
+
+    abstract Double calculateAdminFee();
 }
--- Solid/ocp/DeluxeCinema.java
+++ Solid/ocp/DeluxeCinema.java
@@ -2,4 +2,10 @@ public class DeluxeCinema extends Cinema {
     public DeluxeCinema(double price) {
         this.price = price;
     }
+
+    @Override
+    Double calculateAdminFee() {
+        return price * 12 / 100;
+    }
+
 }
