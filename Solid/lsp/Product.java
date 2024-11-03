6f6cb11cd648b4a30305747f9f5c2899a9d62c03
DESKTOP-RRNUSDH\USER <eycaputri04@gmail.com>
2024-11-03 19:18:50 +0700

    penerapan lsp

--- Solid/lsp/Product.java
+++ Solid/lsp/Product.java
@@ -1,12 +1,11 @@
 public class Product {
     abstract String getName();
 
-    abstract Date getExpiredDate();
-
     /**
      * Function to get all of information about product
      */
     public void getProductInfo() {
         // some magic code
     }
+
 }
